3bde881f3587412005711935ba07632bbdb8cedd
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-10-07 18:53:44 +0100

    build: use main version in setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,27 @@ import pathlib
 from setuptools import setup
 from setuptools.command.install import install
 
+import python_githooks
+
 HERE = pathlib.Path(__file__).parent
 
-VERSION = "1.0.6"
+VERSION = python_githooks.__version__
 
 README = (HERE / "README.md").read_text()
 
 
 class VerifyVersionCommand(install):
     """Command to verify that the git tag matches the library version"""
-    description = 'verify that the git tag matches the library version'
+
+    description = "verify that the git tag matches the library version"
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG').replace('v', '')
+        tag = os.getenv("CIRCLE_TAG").replace("v", "")
 
         if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
             sys.exit(info)
 
 
@@ -42,12 +47,6 @@ setup(
     packages=["python_githooks"],
     include_package_data=True,
     install_requires=["configparser"],
-    entry_points={
-        "console_scripts": [
-            "githooks=python_githooks.__main__:main",
-        ]
-    },
-    cmdclass={
-        'verify': VerifyVersionCommand,
-    }
+    entry_points={"console_scripts": ["githooks=python_githooks.__main__:main"]},
+    cmdclass={"verify": VerifyVersionCommand},
 )
